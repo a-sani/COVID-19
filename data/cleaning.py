64eb21e426d401c15dd15217d08180e55dd84a9e
Jograj Sidhu <jograjsidhu@Jograjs-MacBook-Pro.local>
2021-02-18 18:28:32 -0800

    more functionality to fixAge

--- data/cleaning.py
+++ data/cleaning.py
@@ -6,6 +6,7 @@ import math
 
 #read csv file from command line
 data = pd.read_csv(sys.argv[1])
+data.drop(columns=['additional-information', 'source'])
 
 #fixing the various age formats
 def fixAge(age):
@@ -13,12 +14,17 @@ def fixAge(age):
     range_pat = "(\d+)-(\d+)"
     #pattern to match if range is float or int
     single_pat = "(\d+)"
+    #pattern to match months
+    month_pat = "(\d+) month"
     #create regex patterns
     pat1 = re.compile(range_pat)
     pat2 = re.compile(single_pat)
+    pat3 = re.compile(month_pat)
+
     #match the age format to the regex pattern
     result1 = pat1.match(age)
     result2 = pat2.match(age)
+    result3 = pat3.match(age)
     #if age is a range value, return the average
     if result1:
         i = int(result1.group(1))
@@ -26,9 +32,10 @@ def fixAge(age):
         return math.ceil((i+j)//2)
     #if age is float/int, return int
     if result2:
-        return int(result2.group(1))
-
-
-
+        #if age is in months, consider them 1 years old
+        if result3:
+            return 1
+        else:
+            return int(result2.group(1))
 
 
